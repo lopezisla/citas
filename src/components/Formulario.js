7d0829b9870b64b11fdf3794921b0a12951faa0a
Jorge <jorgelopezisla@gmail.com>
2021-02-05 21:07:18 -0300

    estructure created

---
+++ src/components/Formulario.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+const Formulario = () => {
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+      <form>
+        <label>Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+        />
+        <label>Propietario</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Propietario"
+        />
+        <label>Fecha</label>
+        <input type="date" name="fecha" className="u-full-width" />
+        <label>Hora</label>
+        <input type="time" name="hora" className="u-full-width" />
+        <label>Síntomas</label>
+        <textarea className="u-full-width" name="sintomas"></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Formulario;
